57968993006114917ffdf53e985253786f845cd3
Suraj <surajchopra1603@gmail.com>
2026-02-03 21:21:59 +0530

    Refactor App component to use createBrowserRouter and RouterProvider for routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,44 @@
 /** Imported modules */
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 import Home from "./features/home/Home.tsx";
 import Login from "./features/user/pages/Login.tsx";
 import Signup from "./features/user/pages/Signup.tsx";
 import Header from "./shared/components/Header.tsx";
 import Sidebar from "./shared/components/Sidebar.tsx";
 import APIs from "./features/apis/APIs.tsx";
-import Flow from "./features/flow/Flow.tsx";
 import { Toaster } from "sonner";
 import { AlertDialogProvider } from "./shared/overlays/AlertDialogProvider.tsx";
 import { LoadingOverlayProvider } from "./shared/overlays/LoadingOverlayProvider.tsx";
 import { useEffect } from "react";
 import { useUserStore } from "./features/user/store/user-store.ts";
 import { userAPI } from "./features/user/api/user-api.ts";
+
+/** Browser router */
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <Signup /> },
+    {
+        element: (
+            <div className="flex h-screen w-full overflow-hidden bg-[#F5F5F5]">
+                {/* Sidebar */}
+                <Sidebar />
+
+                {/* Content */}
+                <div className="flex h-full min-w-0 flex-1 flex-col">
+                    <Header />
+                    <div className="mr-2 mb-2 min-h-0 flex-1">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        ),
+        children: [
+            { path: "/apis", element: <APIs /> }
+            // { path: "/flow", element: <Flow /> }
+        ]
+    }
+]);
 
 /** App component */
 const App = () => {
@@ -28,48 +54,15 @@ const App = () => {
     }, [setUser]);
 
     return (
-        <BrowserRouter>
-            <AlertDialogProvider>
-                <LoadingOverlayProvider>
-                    {/* Toaster for notifications */}
-                    <Toaster position="top-right" />
+        <AlertDialogProvider>
+            <LoadingOverlayProvider>
+                {/* Toaster for notifications */}
+                <Toaster position="top-right" />
 
-                    {/* Routes */}
-                    <Routes>
-                        {/* Home route */}
-                        <Route path="/" element={<Home />} />
-
-                        {/* Auth routes */}
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-
-                        {/* App routes */}
-                        <Route
-                            element={
-                                <div className="flex h-screen w-full overflow-hidden bg-[#F5F5F5]">
-                                    {/* Sidebar */}
-                                    <Sidebar />
-
-                                    {/* Content */}
-                                    <div className="flex h-full min-w-0 flex-1 flex-col">
-                                        <Header />
-
-                                        <div className="mr-2 mb-2 min-h-0 flex-1">
-                                            <Outlet />
-                                        </div>
-                                    </div>
-                                </div>
-                            }>
-                            {/* APIs route */}
-                            <Route path="/apis" element={<APIs />} />
-
-                            {/* Flow route */}
-                            <Route path="/flow" element={<Flow />} />
-                        </Route>
-                    </Routes>
-                </LoadingOverlayProvider>
-            </AlertDialogProvider>
-        </BrowserRouter>
+                {/* Router provider */}
+                <RouterProvider router={router} />
+            </LoadingOverlayProvider>
+        </AlertDialogProvider>
     );
 };
 
